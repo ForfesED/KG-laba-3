b8f16bdd8f868990cf73ea5749394f5d96bf4d05
Andrei Kombintsev <forfesed@gmail.com>
2025-12-06 05:23:40 +0300

    Add files via upload

--- niga/model.cpp
+++ niga/model.cpp
@@ -1,42 +1,54 @@
-
-#include "model.h"
+#include <iostream>
+#include <string>
 #include <fstream>
 #include <sstream>
-#include <iostream>
+#include <vector>
+#include "model.h"
 
-Model::Model(const char* filename) {
-    std::ifstream in(filename);
-    if (!in.is_open()) {
-        std::cerr << "Cannot open " << filename << "\n";
-        return;
-    }
+Model::Model(const char *filename) : verts_(), faces_() {
+    std::ifstream in;
+    in.open (filename, std::ifstream::in);
+    if (in.fail()) return;
     std::string line;
-    while (std::getline(in, line)) {
-        std::istringstream iss(line);
+    while (!in.eof()) {
+        std::getline(in, line);
+        std::istringstream iss(line.c_str());
         char trash;
         if (!line.compare(0, 2, "v ")) {
             iss >> trash;
-            float x, y, z;
-            iss >> x >> y >> z;
-            verts.push_back(Vec3f(x, y, z));
-        }
-        else if (!line.compare(0, 2, "f ")) {
+            Vec3f v;
+            for (int i=0;i<3;i++) iss >> v.raw[i];
+            verts_.push_back(v);
+        } else if (!line.compare(0, 2, "f ")) {
+            std::vector<int> f;
+            int itrash, idx;
             iss >> trash;
-            int f;
-            std::vector<int> face;
-            while (iss >> f) {
-                face.push_back(f - 1);
+            while (iss >> idx >> trash >> itrash >> trash >> itrash) {
+                idx--; // in wavefront obj all indices start at 1, not zero
+                f.push_back(idx);
             }
-            faces.push_back(face);
+            faces_.push_back(f);
         }
     }
+    std::cerr << "# v# " << verts_.size() << " f# "  << faces_.size() << std::endl;
 }
 
-int Model::nverts() { return verts.size(); }
-int Model::nfaces() { return faces.size(); }
+Model::~Model() {
+}
 
-Vec3f Model::vert(int i) { return verts[i]; }
+int Model::nverts() {
+    return (int)verts_.size();
+}
 
-std::vector<int> Model::face(int idx) { return faces[idx]; }
+int Model::nfaces() {
+    return (int)faces_.size();
+}
 
-Model::~Model() {}
+std::vector<int> Model::face(int idx) {
+    return faces_[idx];
+}
+
+Vec3f Model::vert(int i) {
+    return verts_[i];
+}
+
--- niga/model.h
+++ niga/model.h
@@ -1,18 +1,20 @@
+#ifndef __MODEL_H__
+#define __MODEL_H__
 
-#pragma once
 #include <vector>
-#include <string>
 #include "geometry.h"
 
 class Model {
-public:
-    Model(const char* filename);
-    int nverts();
-    int nfaces();
-    Vec3f vert(int i);
-    std::vector<int> face(int idx);
-    ~Model();
 private:
-    std::vector<Vec3f> verts;
-    std::vector<std::vector<int>> faces;
+	std::vector<Vec3f> verts_;
+	std::vector<std::vector<int> > faces_;
+public:
+	Model(const char *filename);
+	~Model();
+	int nverts();
+	int nfaces();
+	Vec3f vert(int i);
+	std::vector<int> face(int idx);
 };
+
+#endif //__MODEL_H__
